3c9949a4133e6861f35307faebb30fda4e9a3f1d
gustavodias2@gmail.com <gustavodias2@gmail.com>
2021-08-10 15:11:24 -0300

    Aula 11 - Mapa mental usando ferramenta freplane
     utilizados no readme
    s, iniciando automação web

---
+++ mapaMental/login.mm
@@ -0,0 +1,76 @@
+<map version="freeplane 1.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Login" FOLDED="false" ID="ID_107805750" CREATED="1610381326375" MODIFIED="1628618692777"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt" TEXT_SHORTENED="true">
+<font SIZE="24"/>
+<richcontent CONTENT-TYPE="plain/" TYPE="DETAILS"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_1273250224" ICON_SIZE="12 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120 pt" MIN_WIDTH="120 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1273250224" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="Arial" SIZE="9" BOLD="true" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="3"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" TEXT_ALIGN="LEFT">
+<font SIZE="11" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font SIZE="9" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT">
+<font BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" STYLE="bubble" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1358928635">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#0000ff" TRANSPARENCY="255" DESTINATION="ID_1358928635"/>
+<edge COLOR="#0000cc"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<font SIZE="9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" UNIFORM_SHAPE="true" MAX_WIDTH="120 pt" MIN_WIDTH="120 pt">
+<font SIZE="24" ITALIC="true"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font SIZE="24"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
+<node TEXT="Caminho Feliz" POSITION="right" ID="ID_1305980759" CREATED="1628618697438" MODIFIED="1628618702573">
+<node TEXT="Fechar Modal" ID="ID_829019609" CREATED="1628618726431" MODIFIED="1628618733008">
+<node TEXT="Fora da modal" ID="ID_128272977" CREATED="1628618733014" MODIFIED="1628618737652"/>
+<node TEXT="Ícone fechar" ID="ID_1838277184" CREATED="1628618737868" MODIFIED="1628618745282"/>
+</node>
+<node TEXT="Create New Account" ID="ID_599052930" CREATED="1628618750595" MODIFIED="1628618758780"/>
+<node TEXT="Realizar Login com usuário válido" ID="ID_1954350594" CREATED="1628618759745" MODIFIED="1628618776794">
+<node TEXT="Campos obrigatórios" ID="ID_605507394" CREATED="1628618776802" MODIFIED="1628618782515"/>
+<node TEXT="Todos os campos" ID="ID_322705722" CREATED="1628618783056" MODIFIED="1628618790650"/>
+</node>
+</node>
+<node TEXT="Caminho não tão feliz" POSITION="right" ID="ID_992055774" CREATED="1628618702704" MODIFIED="1628618714634">
+<node TEXT="Login Inválido" ID="ID_1331340471" CREATED="1628618796105" MODIFIED="1628618804145">
+<node TEXT="Usuário inválido" ID="ID_258320741" CREATED="1628618814961" MODIFIED="1628618819404"/>
+<node TEXT="Senha inválida" ID="ID_1960297701" CREATED="1628618819551" MODIFIED="1628618824587"/>
+</node>
+<node TEXT="Campos Obrigatórios" ID="ID_787003799" CREATED="1628618804493" MODIFIED="1628618811512">
+<node TEXT="Usuário em branco" ID="ID_1603064148" CREATED="1628618834890" MODIFIED="1628618839162"/>
+<node TEXT="Senha em branco" ID="ID_910879283" CREATED="1628618839286" MODIFIED="1628618843197"/>
+</node>
+</node>
+</node>
+</map>
