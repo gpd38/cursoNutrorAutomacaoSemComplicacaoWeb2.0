eca7b1f182c14360a7edf3fe38acbb3f72d0093b
Gustavo Dias <gustavodias2@gmail.com>
2021-09-12 21:03:50 -0300

    Renomeando pasta com o codigo fonte

--- codigo/src/test/java/br/com/chronosAcademy/enums/Browser.java
+++
@@ -1,5 +0,0 @@
-package br.com.chronosAcademy.enums;
-
-public enum Browser {
-    CHROME, FIREFOX, EDGE, IE
-}
--- codigo/src/test/java/br/com/chronosAcademy/maps/NewAccountMap.java
+++
@@ -1,9 +0,0 @@
-package br.com.chronosAcademy.maps;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class NewAccountMap {
-    @FindBy(css = ".fixedImportant")
-    public WebElement textCreateAccount;
-}
